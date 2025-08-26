176ff5303750b6a8a6da1ac19faba9e259b94f76
Jay <jdesai840@gmail.com>
2025-08-26 16:30:08 -0400

    Initial Commit

---
+++ skinfluence2/skinfluence2App.swift
@@ -0,0 +1,17 @@
+//
+//  skinfluence2App.swift
+//  skinfluence2
+//
+//  Created by Jay Desai on 8/26/25.
+//
+
+import SwiftUI
+
+@main
+struct skinfluence2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ skinfluence2Tests/skinfluence2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  skinfluence2Tests.swift
+//  skinfluence2Tests
+//
+//  Created by Jay Desai on 8/26/25.
+//
+
+import Testing
+@testable import skinfluence2
+
+struct skinfluence2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
